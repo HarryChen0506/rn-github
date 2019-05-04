9b512bae939b3c3fcb92e1ccab25e732c3b044ae
HarryChen0506 <chenwenhua0506@sina.com>
2019-05-04 16:35:38 +0800

    动态tab路由

--- js/navigators/HomeTopTabNavigator.js
+++ js/navigators/HomeTopTabNavigator.js
@@ -1,64 +1,77 @@
 import React from "react"
 import { StyleSheet, View, Text } from "react-native"
-import {createMaterialTopTabNavigator} from "react-navigation"
-import NavigationUtil, {ROOT_NAVIGATION, TOP_TAB_NAVIGATION, BOTTOM_TAB_NAVIGATION} from '@utils/NavigationUtil'
+import { createMaterialTopTabNavigator } from "react-navigation"
+import NavigationUtil, { ROOT_NAVIGATION, TOP_TAB_NAVIGATION, BOTTOM_TAB_NAVIGATION } from '@utils/NavigationUtil'
 
+const tabNames = ['Java', 'Android', 'Ios', 'React', 'React Native', 'PHP', 'Python']
 class TabContent extends React.Component {
   componentDidMount() {
     // 注册工具路由
-    const {navigation} = this.props
+    const { navigation } = this.props
     NavigationUtil.setNavigation(navigation, TOP_TAB_NAVIGATION)
   }
   render() {
-    const {navigation} = this.props
-    const {state} = navigation
+    const { navigation, item } = this.props
+    const { state } = navigation
     return (
       <View style={styles.container}>
-        <Text style={styles.text}>{state.routeName}</Text>
-        <Text 
-          style={styles.text} 
+        {/* <Text style={styles.text}>{state.routeName}</Text> */}
+        <Text style={styles.text}>{item}</Text>
+        <Text
+          style={styles.text}
           onPress={() => {
             // NavigationUtil.goNavigationPage(ROOT_NAVIGATION, 'DetailPage')
-            NavigationUtil.goPage({navigation: navigation}, 'DetailPage')
+            NavigationUtil.goPage({ navigation: navigation }, 'DetailPage')
           }}>跳转到详情页</Text>
-        <Text 
-          style={styles.text} 
+        <Text
+          style={styles.text}
           onPress={() => {
-            NavigationUtil.goNavigationPage(TOP_TAB_NAVIGATION, 'PopularTab2')
+            NavigationUtil.goNavigationPage(TOP_TAB_NAVIGATION, 'tab2')
           }}>跳转到topTab2页</Text>
-        <Text 
-          style={styles.text} 
+        <Text
+          style={styles.text}
           onPress={() => {
-            NavigationUtil.goPage({navigation: navigation}, 'TrendingPage')
+            NavigationUtil.goPage({ navigation: navigation }, 'TrendingPage')
           }}>跳转到BottomTab trending页</Text>
       </View>
     )
   }
 }
-
+const createRouteConfigs = (tabList = []) => {
+  const config = {}
+  tabList.forEach((item, index) => {
+    config[`tab${index}`] = {
+      screen: (props) => <TabContent {...props} item={item}/>,
+      navigationOptions: {
+        title: item,
+      }
+    }
+  })
+  return config
+}
 const HomeTopTabNavigator = createMaterialTopTabNavigator(
+  createRouteConfigs(tabNames),
   {
-    PopularTab1: {
-      screen: TabContent,
-      navigationOptions: {
-        title: 'tab1'
-      }
-    },
-    PopularTab2: {
-      screen: TabContent,
-      navigationOptions: {
-        title: 'tab2'
-      }
-    },
-  }, {
-    initialRouteName: 'PopularTab1',
+    initialRouteName: 'tab0',
     navigationOptions: {
       // header: null,
     },
     tabBarOptions: {
+      upperCaseLabel: false,
+      scrollEnabled: true,
       style: {
-        marginTop: 30
+        marginTop: 30,
       },
+      tabStyle: {
+        minWidth: 50,
+        width: 120,
+        height: 50,
+      },
+      indicatorStyle: {
+        height: 2,
+        backgroundColor: '#fff',
+      },
+      
     }
   }
 )
