f47ff59250699b5ea2a6fd6b402f5fff503b12c3
HarryChen0506 <chenwenhua0506@sina.com>
2019-05-04 22:14:01 +0800

    redux logger

--- js/model/store/index.js
+++ js/model/store/index.js
@@ -1,11 +1,13 @@
 import { applyMiddleware, createStore } from 'redux'
 import thunk from 'redux-thunk'
+import logger from 'redux-logger'
 import reducers from '../reducers'
 import { middleware } from '@navigators/AppNavigator'
 
 const middlewares = [
   middleware,
   thunk,
+  logger,
 ]
 
 /**
